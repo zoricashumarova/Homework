a6fc9b68022a546677aa00869d5a2e6c37503c8f
zoricashumarova <74137686+zoricashumarova@users.noreply.github.com>
2023-10-03 16:20:37 +0200

    Create zadaca12.c

---
+++ Tema3/zadaca12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+12. Напиши програма каде преку тастатура ќе може да се внесат два цели броеви.
+Да се отпечати на екран помалиот од броевите - “Brojot %d e pomal od brojot %d”.
+*/
+
+int main()
+{
+    printf("Vnesi dva celi broja!\n");
+    int number1, number2;
+    scanf("%d", &number1);
+    scanf("%d", &number2);
+
+    if(number1 < number2) {
+        printf("Brojot %d e pomal od brojot %d", number1, number2);
+    } else {
+        printf("Brojot %d e pomal od brojot %d", number2, number1);
+    }
+    return 0;
+}
